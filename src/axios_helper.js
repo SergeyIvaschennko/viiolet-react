c9c153f487485d3c2c1428cb6faf6290241036f4
SergeyIvaschennko <boss.awdr@mail.ru>
2024-05-03 13:37:39 +0300

    Works with roles

---
+++ src/axios_helper.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+
+export const getAuthToken = () => {
+    return window.localStorage.getItem('auth_token');
+};
+
+export const setAuthHeader = (token) => {
+    if (token !== null) {
+        window.localStorage.setItem("auth_token", token);
+    } else {
+        window.localStorage.removeItem("auth_token");
+    }
+};
+
+
+export const getRoleFromToken = () => {
+    const token = getAuthToken();
+    if (token) {
+        const decodedToken = jwtDecode(token);
+        return decodedToken.role;
+    }
+    return null;
+};
+
+axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+export const request = (method, url, data) => {
+
+    let headers = {};
+    if (getAuthToken() !== null && getAuthToken() !== "null") {
+        headers = {'Authorization': `Bearer ${getAuthToken()}`};
+    }
+
+    return axios({
+        method: method,
+        url: url,
+        headers: headers,
+        data: data});
+};
